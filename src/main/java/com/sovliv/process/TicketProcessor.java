aee4f225a737352426e695099c6ad31460074972
thegreatestacc <67763738+thegreatestacc@users.noreply.github.com>
2022-04-06 19:41:54 +0300

    Small changes

--- src/main/java/com/sovliv/process/TicketProcessor.java
+++ src/main/java/com/sovliv/process/TicketProcessor.java
@@ -11,14 +11,6 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-/**
- * Напишите программу на языке программирования java, которая прочитает файл tickets.json и рассчитает:
- * - среднее время полета между городами Владивосток и Тель-Авив
- * - 90-й процентиль времени полета между городами Владивосток и Тель-Авив
- * Программа должна вызываться из командной строки Linux, результаты должны быть представлены в текстовом виде.
- * В качестве результата нужно прислать ответы на поставленные вопросы и ссылку на исходный код.
- */
-
 public class TicketProcessor {
 
     private final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yy HH:mm");
